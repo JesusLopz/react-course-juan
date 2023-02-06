7350e5a9bdf4a6106d4d36c459fe1dc20459044c
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-06 09:11:46 -0700

    const variables finished

---
+++ FundamentosJS/js/02.js
@@ -0,0 +1,15 @@
+// const variables
+
+const client = "Juan";
+const discountPrice = 200;
+
+// it cannot be reasigned
+
+client = "Pablo";
+
+//it cannot be innitialized without a value
+
+const price;
+
+console.log(client);
+console.log(discountPrice);
