4299c64dddfd7400b1ad7723f8f6608cbdfa0e7c
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-06 08:58:50 -0700

    let variable

---
+++ FundamentosJS/js/01.js
@@ -0,0 +1,13 @@
+//variables - let const
+
+let client = "Jesus";
+let discountPrice = 200;
+
+console.log(client);
+console.log(discountPrice);
+
+//reasign
+
+client = 200;
+
+console.log(client);
