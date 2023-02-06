9961405a33480a8213cd2dc640cedc09ed6a55e1
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-06 11:30:48 -0700

    finish object section

--- FundamentosJS/js/04.js
+++ FundamentosJS/js/04.js
@@ -1,11 +1,39 @@
 //Objects
 
-const name = "Tablet";
-const price = 300;
-const disponibility = true;
+// const name = "Tablet";
+// const price = 300;
+// const disponibility = true;
 
 const product = {
-    name: "Tabler",
-    price: 300,
-    disponibility: true
+  nameProduct: "Tablet",
+  price: 300,
+  disponibility: true,
+};
+
+console.log(product);
+console.table(product);
+
+console.log(product.nameProduct);
+console.log(product.price);
+console.log(product.disponibility);
+
+// Destructuring
+
+const { nameProduct, price, disponibility } = product;
+console.log(nameProduct);
+console.log(price);
+console.log(disponibility);
+
+// Object literal enhancement
+
+const auth = true;
+const user = "Juan";
+
+const newObject = {
+    // auth: auth,
+    // user: user
+    auth,
+    user
 }
+
+console.table(newObject)
