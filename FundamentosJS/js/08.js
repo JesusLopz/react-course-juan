3617ad466f05bd532df7ddd5cc926cd251f950d7
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-07 08:33:58 -0700

    arrays finished

---
+++ FundamentosJS/js/08.js
@@ -0,0 +1,12 @@
+const tecnologies = [20, 30, true, "React", "Javascript"];
+
+console.log(tecnologies);
+console.table(tecnologies);
+console.log(tecnologies[2]);
+console.log(tecnologies[3]);
+console.log(tecnologies[4]);
+
+
+
+console.log(tecnologies.length);
+console.log(tecnologies.toString());
