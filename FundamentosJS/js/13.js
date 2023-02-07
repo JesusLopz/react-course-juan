f008899f11ec271b734b8084d364ce1de0f1f3c2
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-07 14:10:09 -0700

    function expression finish

---
+++ FundamentosJS/js/13.js
@@ -0,0 +1,13 @@
+// Functions - Functions Exression
+
+// function sum(num1, num2 = 0){ // <- Function Declaration
+//     console.log(num1 + num2);
+// }
+
+const sum = function (num1, num2) {
+  return num1 + num2;
+};
+
+// sum(11,33); <- Function Declaration
+const result = sum(11,33);
+console.log(result);
