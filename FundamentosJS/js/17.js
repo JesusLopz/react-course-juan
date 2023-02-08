b69677bd921772003ccc03fe6c9b4bf16fbd4a19
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-08 10:15:13 -0700

    conditional if finished

---
+++ FundamentosJS/js/17.js
@@ -0,0 +1,20 @@
+// Conditionals
+
+const moneyAvailable = 400;
+const withdraw = 200;
+
+if (moneyAvailable > withdraw){
+    // If the condition is met executes the code below
+    console.log("You can withdraw your money")
+}else{
+    // If the condition doesnt met, executes the code below
+    console.log("Sorry, insuficent funds")
+}
+
+
+/*
+> - Greater than
+< - Less than
+>= - Greater than or equal to
+<= - Less thn or equal to
+*/
