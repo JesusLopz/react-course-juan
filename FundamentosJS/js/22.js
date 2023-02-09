8ac15abc8512395f38676b18ecffb12d6384f0d8
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-09 08:35:49 -0700

    querySelector finish

---
+++ FundamentosJS/js/22.js
@@ -0,0 +1,9 @@
+// DOM Selectors - querySelector
+
+const heading = document.querySelector('#heading');
+
+console.log(heading);
+console.log(heading.tagName);
+console.log(heading.textContent);
+console.log(heading.classList)
+console.log(heading.id);
