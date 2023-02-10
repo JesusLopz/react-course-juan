161ce7829c40afaa4ab89a9e42022d3ab1b7a3c1
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-10 09:14:11 -0700

    DOM events - inputs finished

---
+++ FundamentosJS/js/25.js
@@ -0,0 +1,19 @@
+// DOM events - inputs
+
+const inputName = document.querySelector('.nombre');
+
+
+inputName.addEventListener('input', function(e){
+    console.log(e.target.value);
+})
+
+const inputPassword = document.querySelector('.password');
+
+inputPassword.addEventListener('input', functionPassword);
+
+function functionPassword(){
+    inputPassword.type = "text"
+
+    setTimeout(() => {
+        inputPassword.type = "password"}, 500)
+}
