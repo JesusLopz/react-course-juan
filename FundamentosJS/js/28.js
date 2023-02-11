9982cb2c3713d3859dfbe5c99c89bf6753ab8d7e
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-11 11:07:39 -0700

    import export

---
+++ FundamentosJS/js/28.js
@@ -0,0 +1,11 @@
+// Import & Export
+
+// import sum from './functions.js'
+import { sum, minus } from "./functions.js"
+
+
+const result = sum(11,11);
+const result2 = minus(33,22);
+
+console.log(result);
+console.log(result2)
---
+++ FundamentosJS/js/functions.js
@@ -0,0 +1,14 @@
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+function minus(num1,num2){
+    return num1 - num2
+}
+
+// export default sum
+export {
+    sum,
+    minus
+}
