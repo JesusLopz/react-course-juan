5e74f282fbbe30041c2a831f8899183c605b84f6
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2023-02-14 10:05:27 -0700

    add list patients and form components

--- appointment-react/src/App.jsx
+++ appointment-react/src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import Form from "./components/Form"
+import PatientList from "./components/PatientList"
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Form />
+      <PatientList />
     </div>
   )
 }
---
+++ appointment-react/src/components/Form.jsx
@@ -0,0 +1,7 @@
+const Form = () => {
+    return(
+        <h2>Hello from Form</h2>
+    )
+}
+
+export default Form;
